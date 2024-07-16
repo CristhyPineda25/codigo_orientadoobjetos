6db00e7dcd59dc419e85bcf12616be375eea558d
CristhyPineda25 <pinedasoad25@gmail.com>
2024-07-16 06:57:27 +0200

    Add files via upload

---
+++ Deportista.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.deportista;
+
+/**
+ *
+ * @author pined
+ */
+public class Deportista extends Persona {
+    private String deporte;
+
+    public Deportista(String nombre, int edad, String deporte) {
+        super(nombre, edad);
+        this.deporte = deporte;
+    }
+
+    public String getDeporte() {
+        return deporte;
+    }
+
+    public void setDeporte(String deporte) {
+        this.deporte = deporte;
+    }
+
+    @Override
+    public void mostrarInfo() {
+        super.mostrarInfo();
+        System.out.println("Deporte: " + deporte);
+    }
+}
